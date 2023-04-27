7fa12cad90547335e1a6bc6e968e52de26bf2f74
erwanCdc <erwan.cadic@outlook.fr>
2023-04-27 12:27:42 +0200

    Four boards and titles displayed for each one

--- src/Display.py
+++ src/Display.py
@@ -47,10 +47,15 @@ class Display:
             for col in range(self.solver.grid_dim)
         ]
 
-    def draw_board(self, board):
+    def draw_board(self, board, grid_name):
         for col in range(self.solver.grid_dim):
             for row in range(self.solver.grid_dim):
                 pygame.draw.rect(self.screen, (0, 0, 0), board[col][row], 1)
+
+        font = pygame.font.Font(None, 24)
+        title_pos = ((board[2][0])[0] + int(self.cell_size/2), (board[2][0])[1] - int(self.cell_size/5))
+        self.draw_text(font, f"{grid_name}", title_pos[0], title_pos[1])
+  
 
     def draw_agents(self, mode="current"):
         assert mode == "current" or mode == "target"
@@ -67,7 +72,7 @@ class Display:
             pygame.draw.rect(
                 self.screen,
                 (
-                    255,
+                    255 - normalized_distance,
                     255 - normalized_distance,
                     255 - normalized_distance,
                 ),
@@ -76,9 +81,21 @@ class Display:
             )
 
     def draw_heatmap(self):
-        board = self.board_distances
-
-
+        board = self.board_heatmap
+        for agent in self.solver.agents:
+            pos = agent.current_pos
+            weight = 3 * agent.heatmap[agent._id]
+            if (weight <= 255):
+                color = (255, 255 - (weight), 255 - (weight))
+            else:
+                # cell turns black if the agent occupying it has been blocking others for too many iterations.
+                color = (255, 0, 0)
+            pygame.draw.rect(
+                self.screen,
+                color,
+                board[pos[0]][pos[1]],
+                0,
+            ) 
 
     def draw_text(self, font, text, center_col, center_row, color=(0, 0, 0)):
         text_surface = font.render(text, True, color)
@@ -87,23 +104,23 @@ class Display:
         self.screen.blit(text_surface, text_rect)
 
     def draw_simulation_info(self):
-        font = pygame.font.Font(None, 24)
-
+        font = pygame.font.Font(None, 18)
+        text_center_position = int(self.screen_size*2.3/4)
         offset_col = self.screen_size // 2
 
         # grid size
-        self.draw_text(font, "GRID SIZE", offset_col, 50)
+        self.draw_text(font, "GRID SIZE", offset_col, int(text_center_position-0.15*text_center_position))
         self.draw_text(
-            font, f"{self.solver.grid_dim} x {self.solver.grid_dim}", offset_col, 70
+            font, f"{self.solver.grid_dim} x {self.solver.grid_dim}", offset_col, int(text_center_position-0.1*text_center_position)
         )
 
         # number of agents
-        self.draw_text(font, "NUMBER OF AGENTS", offset_col, 110)
-        self.draw_text(font, f"{int(self.solver.nb_agents)}", offset_col, 130)
+        self.draw_text(font, "NUMBER OF AGENTS", offset_col, text_center_position)
+        self.draw_text(font, f"{int(self.solver.nb_agents)}", offset_col, int(text_center_position+0.05*text_center_position))
 
         # number of moves
-        self.draw_text(font, "MOVES COUNT", offset_col, 170)
-        self.draw_text(font, f"{self.solver.stats['moves_count']}", offset_col, 190)
+        self.draw_text(font, "MOVES COUNT", offset_col, int(text_center_position+0.15*text_center_position))
+        self.draw_text(font, f"{self.solver.stats['moves_count']}", offset_col, int(text_center_position+0.2*text_center_position))
 
     def update(self):
         # Update the game display based on the game state
@@ -112,9 +129,9 @@ class Display:
         self.draw_agents("target")
         self.draw_distances()
         self.draw_heatmap()
-        self.draw_board(self.board_agents)
-        self.draw_board(self.board_target)
-        self.draw_board(self.board_distances)
-        self.draw_board(self.board_heatmap)
+        self.draw_board(self.board_agents, "Agents Positions")
+        self.draw_board(self.board_target, "Agents Targets")
+        self.draw_board(self.board_distances, "Agents distance from target")
+        self.draw_board(self.board_heatmap, "Agents blocking Heatmap")
         self.draw_simulation_info()
         pygame.display.flip()
